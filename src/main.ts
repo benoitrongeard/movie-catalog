b06308606813a9d1a936fa7a110d494f690d46a5
Rongeard Benoit <benoit.rongeard@gmail.com>
2023-12-11 23:04:14 +0100

    Adding vercel Analytics

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { injectSpeedInsights } from '@vercel/speed-insights';
+import { inject } from '@vercel/analytics';
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch(err => console.error(err))
   .finally(() => {
-    console.log('Activate Vercel Speed Insights');
+    console.log('Activate Vercel Speed Insights & Analytics');
     injectSpeedInsights({});
+    inject();
   });
