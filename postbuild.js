2fb172dcd3a4487339af65502766fad775dd6259
Rongeard Benoit <benoit.rongeard@gmail.com>
2023-12-11 22:48:50 +0100

    Adding vercel speed insights

--- postbuild.js
+++ postbuild.js
@@ -1,18 +1,28 @@
 const fs = require('fs');
 
 // Reading config file
-fs.readFile('./dist/movie-catalog/assets/config/config.template.json', 'utf8', function(err, data) {
-
-    console.log("DEBUUUUUUUG");
+fs.readFile(
+  './dist/movie-catalog/assets/config/config.template.json',
+  'utf8',
+  function (err, data) {
     if (err) {
-        return console.log(err);
+      return console.log(err);
     }
 
     // Replace env value
-    var result = data.replace(/@\[TMDB_API_URL\]/g, process.env.TMDB_API_URL).replace(/@\[TMDB_API_TOKEN\]/g, process.env.TMDB_API_TOKEN);
+    var result = data
+      .replace(/@\[TMDB_API_URL\]/g, process.env.TMDB_API_URL)
+      .replace(/@\[TMDB_API_TOKEN\]/g, process.env.TMDB_API_TOKEN)
+      .replace(/@\[PRODUCTION\]/g, process.env.PRODUCTION);
 
     // Update the file
-    fs.writeFile('./dist/movie-catalog/assets/config/config.json', result, 'utf8', function(err) {
+    fs.writeFile(
+      './dist/movie-catalog/assets/config/config.json',
+      result,
+      'utf8',
+      function (err) {
         if (err) return console.log(err);
-    });
-});
+      }
+    );
+  }
+);
